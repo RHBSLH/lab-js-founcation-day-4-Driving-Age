51009f5f4488301d3fa9a6dc22c057b9ae8289c3
RHBSLH <Rehab.Saleh.abt.js@tuwaiq.edu.sa>
2021-10-13 10:33:54 +0300

    lab js founction

---
+++ Jsfounction.js
@@ -0,0 +1,11 @@
+let age = 12
+let allwedage = 18 - age
+if (age < 18) {
+
+    console.log("Sorry, you can't drive yet")
+    console.log("Sorry, you have " + allwedage + " years to wait until you can drive")
+} else {
+
+    console.log("You can drive, nice car by the way")
+}
+
